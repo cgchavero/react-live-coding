6132c899d37b550a9f7d61a9b1a72defc82bb62a
Cesar Granjeno <cgchavero@gmail.com>
2022-07-23 00:58:02 -0500

    initial commit

---
+++ src/components/ListOfGifs.js
@@ -0,0 +1,34 @@
+import React, {useState, useEffect} from 'react'
+import Gif from './Gif'
+import getGifs from '../services/getGifs'
+
+export default function ListOfGifs ({params})
+{
+    const {keyword} = params
+    const [loading, setLoading] = useState(false)
+    const [gifs, setGifs] = useState([])
+
+    useEffect(function(){
+        setLoading(true)
+        getGifs({keyword})
+            .then(gifs => {
+                setGifs(gifs)
+                setLoading(false)
+            })
+      }, [keyword])
+
+      if (loading) return <i>Cargando...🌀</i>
+
+    return <div>
+        {
+            gifs.map(({id, title, url}) => 
+                <Gif
+                    id={id}
+                    key={id}
+                    title={title}
+                    url={url}
+                /> 
+            )
+        }
+    </div>
+}
